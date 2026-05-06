2cbcad7e65a812d4e7b76f1107e69d381a69b1cd
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-05-06 13:33:35 -0300

    Estruturação dos diretorios do projeto

---
+++ streaming-dominio-publico/src/main/java/br/com/voltantes/bd2/streaming_dominio_publico/StreamingDominioPublicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.voltantes.bd2.streaming_dominio_publico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingDominioPublicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingDominioPublicoApplication.class, args);
+	}
+
+}
